d9b989d856dc1c25b493fe46a8fc13a80cb5d93a
Marrier-Axel <marrieraxel@gmail.com>
2026-03-03 12:57:50 +0100

    ajout de l'éditeur

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 import { Wall } from "./class/Wall.js";
 import { Canvas } from "./class/Canvas.js";
 import { Ball } from "./class/Ball.js";
-
-const width = 750;
-const height = 515;
-let canvas = new Canvas(width, height);
-
 const balls = []
 const walls = []
 const objects = []
 
-let editor_mode = true
+const width = 750;
+const height = 515;
+let editor_mode = true 
+let canvas = new Canvas(width, height);
 
 document.addEventListener("DOMContentLoaded", function(){
+    eventHandler();
+    
+    // INIT CANVA
     canvas.bgChange(200, 200, 200);
 
     let floor = new Wall(width, height, 0, height);
@@ -21,28 +22,11 @@ document.addEventListener("DOMContentLoaded", function(){
     let ceiling = new Wall(0, 0, width, 0);
 
     walls.push(floor, wall1, wall2, ceiling);
-    
-    objects.push(...walls);
 
-    objects.forEach(object => { //dessine les objets
+    walls.forEach(object => { //dessine les objets
         object.draw(canvas);
     });
-
-    canvas.canvas.addEventListener("mousemove",(e)=>{
-        document.querySelector('#posX mark').innerHTML = e.offsetX
-        document.querySelector('#posY mark').innerHTML = e.offsetY
-    })
-
-    SetupControlUI();
 })
-
-function SetupControlUI(){
-    console.log("Début du setup de l'UI pour le controle de la simulation")
-    let btnStart = document.createElement("button")
-    document.querySelector("body").appendChild(btnStart)
-    btnStart.innerText = "Démarrer la simulation"
-    btnStart.addEventListener("click", debutSimulation)
-}
 
 function update(){
     canvas.bgChange(200, 200, 200); //efface l'ecran
@@ -69,12 +53,12 @@ function update(){
         });
     }
     
-    
-    
 }
+
 
 function debutSimulation(){
     if(editor_mode == true){
+        console.log("Début de la simultation")
         editor_mode = false
 
         let nb_balls = Number(document.querySelector('#nbBalls input').value);
@@ -84,10 +68,60 @@ function debutSimulation(){
             balls.push(ball);
         }
 
-        objects.push(...balls);
+        
+        objects.push(...balls, ...walls);
 
         setInterval(() => {
             update()
         }, 10);
-    }     
+    }    
+}
+
+
+
+
+
+function eventHandler(){
+    //Bouton démarrer
+    document.querySelector("#DebutSimu").addEventListener('click', debutSimulation);
+
+    //Conteur de balles
+    document.querySelector("#nbBalls input").addEventListener('input', updateBallCounter);
+
+    //Détection click pour les balles
+    canvas.canvas.addEventListener("click", function(e){
+        if(document.querySelector('#editorSelect').value == "ball"){
+            let ball = new Ball(e.offsetX, e.offsetY, 0, 0, 10)
+            balls.push(ball);
+            ball.draw(canvas);
+        }
+    })
+
+    let wallStep = 0
+    let wallX1 = 0
+    let wallY1 = 0
+    //Détection click pour les murs 1
+    canvas.canvas.addEventListener("mousedown", function(e){
+        if(document.querySelector('#editorSelect').value == "wall"){
+            if(wallStep == 0){
+                wallStep = 1
+                wallX1 = e.offsetX
+                wallY1 = e.offsetY
+            } else{
+                wallStep = 0
+                let newWall1 = new Wall(wallX1, wallY1, e.offsetX, e.offsetY);
+                let newWall2 = new Wall(e.offsetX, e.offsetY, wallX1, wallY1);
+
+                newWall1.draw(canvas)
+                newWall2.draw(canvas)
+
+                walls.push(newWall1)
+                walls.push(newWall2)
+            }
+        }
+    })
+}
+
+function updateBallCounter(){
+    document.querySelector('#nbBalls label').innerHTML = document.querySelector("#nbBalls input").value;
 }
